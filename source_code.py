c46b92f54e1581801106f333a6f19476c4b597d7
rujulzalte <rujul.z@consultadd.com>
2023-01-31 01:57:02 +0530

    updated source code

--- source_code.py
+++ source_code.py
@@ -1,2 +1,11 @@
 print("Hello world")
 print("this is for training")
+print("new line added")
+
+def feature1():
+     print("new line")
+     print("this is a new feature")
+     return None
+
+def feature2():
+     print("new feature")
